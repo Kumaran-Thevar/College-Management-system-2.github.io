089bf47605a82b8b871783c3202e67e979d34d3f
Gamers-Arena <85639617+Gamers-Arena@users.noreply.github.com>
2022-04-11 23:10:27 +0530

    commit

--- App.py
+++ App.py
@@ -1,9 +1,10 @@
-#### App.py code
+
 
 from flask import Flask, render_template, Response, request, redirect, url_for
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template('Home.html');
-
